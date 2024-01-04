07ef66ffc7ae4480c62e2bac69f485721e03d8f5
Hamdi Adan <hhhaji013@gmail.com>
2024-01-04 20:06:22 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,46 +1,16 @@
 // Code your solutions in this file//
-
-function writeCards(names, eventName) {
-
-    let thankYouMessages = [];
-  
-    
-    for (let i = 0; i < names.length; i++) {
-      
-      let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
-  
-      
-      thankYouMessages.push(message);
-    }
-  
-    
-    return thankYouMessages;
+function writeCards(namesArray,event){
+  let thankYouCard =[];
+  for(let i =0 ; i < namesArray.length ; i ++){
+    thankYouCard.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
   }
-  
-  
-  const namesArray = ["Ada", "Brendan", "Ali"];
-  const event = "Birthday";
-  const messages = writeCards(namesArray, event);
-  
-  
-  console.log(messages);
-  
-
-
-function countDown(startingNumber) {
-    
-    if (typeof startingNumber !== 'number' || startingNumber <= 0) {
-      console.log("Please provide a positive integer as an input.");
-      return;
-    }
-  
-    
-    while (startingNumber >= 0) {
-      console.log(startingNumber);
-      startingNumber--;
-    }
+return thankYouCard;
+}
+function countDown(startingNumber){
+  while (startingNumber > 0){
+    console.log(startingNumber);
+    startingNumber-=1;
   }
-  
-  
-  countDown(10)
-  
+  console.log(startingNumber);
+
+}
